5c29a1842cbdc0042159a57ab102a5c47ece6b6e
Reynan Kanindot <ekahiyang@gmail.com>
2022-02-05 15:32:58 +1100

    started index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Employee = require('./lib/employeeClass');
+const Engineer = require('./lib/engineerClass');
+const Manager = require('./lib/managerClass');
+const Intern = require('./lib/internClass');
+
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
