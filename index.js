783ce3c5ebdd9a35a70f63d58d50be0142de6480
Reynan Kanindot <ekahiyang@gmail.com>
2022-02-07 11:25:26 +1100

    data retrieval

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//const Employee = require('./lib/employeeClass');
 const Engineer = require('./lib/engineerClass');
 const Manager = require('./lib/managerClass');
 const Intern = require('./lib/internClass');
@@ -91,118 +90,120 @@ internPrompts = [
     },
 ]
 
-const HTMLfile = `<!DOCTYPE html>
-<html lang="en">
+const htmlFileStart = `<!DOCTYPE html>
+ <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./assets/css/reset.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <title>Document</title>
-</head>
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="./assets/css/reset.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+     <link rel="stylesheet" href="./assets/css/style.css">
+     <title>Document</title>
+ </head>
 
-<body class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation ">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-            </a>
+ <body class="container">
+     <nav class="navbar" role="navigation" aria-label="main navigation ">
+         <div class="navbar-brand">
+             <a class="navbar-item" href="https://bulma.io">
+                 <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+             </a>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
+             <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                 data-target="navbarBasicExample">
+                 <span aria-hidden="true"></span>
+                 <span aria-hidden="true"></span>
+                 <span aria-hidden="true"></span>
+             </a>
+         </div>
 
-        <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item">
-                    Home
-                </a>
+         <div id="navbarBasicExample" class="navbar-menu">
+             <div class="navbar-start">
+                 <a class="navbar-item">
+                     Home
+                 </a>
 
-                <a class="navbar-item">
-                    Documentation
-                </a>
+                 <a class="navbar-item">
+                     Documentation
+                 </a>
 
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        More
-                    </a>
+                 <div class="navbar-item has-dropdown is-hoverable">
+                     <a class="navbar-link">
+                         More
+                     </a>
 
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            About
-                        </a>
-                        <a class="navbar-item">
-                            Jobs
-                        </a>
-                        <a class="navbar-item">
-                            Contact
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">
-                            Report an issue
-                        </a>
-                    </div>
-                </div>
-            </div>
+                     <div class="navbar-dropdown">
+                         <a class="navbar-item">
+                             About
+                         </a>
+                         <a class="navbar-item">
+                             Jobs
+                         </a>
+                         <a class="navbar-item">
+                             Contact
+                         </a>
+                         <hr class="navbar-divider">
+                         <a class="navbar-item">
+                             Report an issue
+                         </a>
+                     </div>
+                 </div>
+             </div>
 
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-primary">
-                            <strong>Sign up</strong>
-                        </a>
-                        <a class="button is-light">
-                            Log in
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
+             <div class="navbar-end">
+                 <div class="navbar-item">
+                     <div class="buttons">
+                         <a class="button is-primary">
+                             <strong>Sign up</strong>
+                         </a>
+                         <a class="button is-light">
+                             Log in
+                         </a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </nav>
 
-    <main class="container is-relative ">
-        <section id="managerContainer" class="columns box is-fullwidth columns is-centered is-bordered">
-            
-        </section>
-        <section id="engineerContainer" class="columns box is-fullwidth is-centered is-bordered ">
-
-        </section>
-        <section id="internContainer" class="box is-fullwidth columns is-centered is-bordered">
-
-        </section>
-    </main>
-
-    
-</body>
-
-</html>`;
+     <main class="container is-relative ">`;
 
 
-const dataBox = `<div class="box bd-notification is-primary column is-one-fifth is-bordered">
-<ul class="">
-    <li>
-        <ul>
-            <li><span class="container title is-5-fullhd">Name</span></li>
-            <li><span class="container title is-4-fullhd">title</span></li>
-        </ul>
-    </li>
-    <li>
-        <ul>
-            <li><span class="box">ID</span></li>
-            <li><span class="box">Email</span></li>
-            <li><span class="box">Office Number</span></li>
-        </ul>
-    </li>
+let htmlMan;
 
-</ul>
-</div>`;
+
+// const htmlManEnd = `</section>`;
+
+// const htmlEngStart = `<section id="engineerContainer" class="columns box is-fullwidth is-centered is-bordered ">`;
+
+// const htmlEngEnd = `</section>`;
+
+// const htmlIntStart = `<section id="internContainer" class="box is-fullwidth columns is-centered is-bordered">`;
+
+// const htmlIntEnd = `</section>`;
+
+// const htmlEnd = `</main > </body ></html > `;
+
+
+// const dataBox = `< div class="box bd-notification is-primary column is-one-fifth is-bordered" >
+//     <ul class="">
+//         <li>
+//             <ul>
+//                 <li><span class="container title is-5-fullhd">Name</span></li>
+//                 <li><span class="container title is-4-fullhd">title</span></li>
+//             </ul>
+//         </li>
+//         <li>
+//             <ul>
+//                 <li><span class="box">ID</span></li>
+//                 <li><span class="box">Email</span></li>
+//                 <li><span class="box">Office Number</span></li>
+//             </ul>
+//         </li>
+
+//     </ul>
+// </div > `;
+
 
 
 
@@ -217,17 +218,8 @@ function saveFile(personnelData, fileName) {
 };
 
 
-let str = HTMLfile;
-console.log(typeof HTMLfile);
-str += dataBox;
-console.log(str);
 
-function writeToFile(str) {
 
-    fs.writeFile('./dist/team.html', str, (err) =>
-        err ? console.log(err) : console.log('Success!')
-    );
-}
 
 
 
@@ -235,6 +227,8 @@ let manager;
 let intern = [];
 let engineer = [];
 let engineerFile = [];
+
+let managerGlb = {};
 
 
 async function main() {
@@ -244,6 +238,9 @@ async function main() {
     let personnelData = await inquirer.prompt(managerPrompts);
 
     manager = new Manager(personnelData.name, personnelData.ID, personnelData.email, personnelData.unique);
+
+    managerGlb = manager.getRole();
+
 
     let answer = false;
     let eCntr = 0;
@@ -256,10 +253,7 @@ async function main() {
 
             let personnelData = await inquirer.prompt(engineerPrompts);
             engineerFile.push(personnelData);
-
             engineer[eCntr] = new Engineer(personnelData.name, personnelData.ID, personnelData.email, personnelData.unique);
-
-
             eCntr++;
 
         }
@@ -267,13 +261,8 @@ async function main() {
         else if (mainMenuChoice.menuSelection == 'Add an Intern') {
 
             let personnelData = await inquirer.prompt(internPrompts);
-
-
             intern[iCntr] = new Intern(personnelData.name, personnelData.ID, personnelData.email, personnelData.unique);
-
-
             iCntr++;
-
         }
         else {
             answer = true;
@@ -281,90 +270,121 @@ async function main() {
 
     } while (answer === false);
 
-    //console.log(manager);
-    //console.log(engineer);
-    //console.log(intern);
-    //console.log(engineerFile);
+    console.log(manager);
+    console.log(engineer);
+    console.log(intern);
+    console.log(engineerFile);
 
-    let saveToFile = saveFile(manager, 'manager');
-    saveToFile = saveFile(engineerFile, 'engineer');
-    saveToFile = saveFile(intern, 'intern');
-    writeToFile(str);
+    let saveToFile = saveFile(manager, 'manager');  // Save Mngr details
+    saveToFile = saveFile(engineerFile, 'engineer');  // Save Engr details
+    saveToFile = saveFile(intern, 'intern');  // Save Intrn details
+    
+
+    
+
+
+
+
+    writeToFile(HTMLfile);
+
+   
 
 
 
 
 
 }
+
+
 
 
 main();
+
+// const htmlMan =
+//     `< div class="box bd-notification is-primary column is-one-fifth is-bordered" >
+// <ul class="">
+//   <li>
+//       <ul>
+//           <li><span class="container title is-5-fullhd">${managerName}</span></li>
+//           <li><span class="container title is-4-fullhd">Manager</span></li>
+//       </ul>
+//   </li>
+//   <li>
+//       <ul>
+//           <li><span class="box"></span></li>
+//           <li><span class="box"></span></li>
+//           <li><span class="box"></span></li>
+//       </ul>
+//   </li>
+
+// </ul>
+// </div >`;
+
+
+const htmlManStart = `
+<section id="managerContainer" class="columns box is-fullwidth columns is-centered is-bordered">`;
+
+
+
+// let htmlEngr;
+
+// htmlEngrInt = () => {
+//     for (let i = 0; i < engineer.length; i++) {
+//         htmlEngr +=
+//             `< div class="box bd-notification is-primary column is-one-fifth is-bordered" >
+//     <ul class="">
+//             <li>
+//             <ul>
+//                 <li><span class="container title is-5-fullhd">${engineer[i].name}</span></li>
+//                 <li><span class="container title is-4-fullhd">Manager</span></li>
+//             </ul>
+//         </li>
+//         <li>
+//             <ul>
+//                 <li><span class="box">${engineer[i].ID}</span></li>
+//                 <li><span class="box">${engineer[i].email}</span></li>
+//                 <li><span class="box">${engineer[i].unique}</span></li>
+//             </ul>
+//         </li>
+
+//     </ul>
+// </div > `;
+//     }
+
+// }
+
+
+
+let HTMLfile = htmlFileStart;
+HTMLfile += htmlManStart;
+//HTMLfile += htmlMan;
+// HTMLfile += htmlManEnd;
+// HTMLfile += htmlEngStart;
+// HTMLfile += htmlEngr
+// HTMLfile += htmlEngEnd;
+// HTMLfile += htmlIntStart;
+// HTMLfile += //Inser iNTERN details here
+//     HTMLfile += htmlIntEnd;
+// HTMLfile += htmlEnd;
+
+
+function writeToFile(HTMLfile) {
+    console.log(HTMLfile);
+    fs.writeFile('./dist/team.html', HTMLfile, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+    
+    
+    console.log(manager.name);
+    
+}
+
+
+
+
 module.exports = { manager, engineer, intern };
 
 
-
-boxTemplate = () => {
-    const managerWindow = document.getElementById('managerContainer');
-    const engineerWindow = document.getElementById('engineerContainer');
-    const internWindow = document.getElementById('internContainer');
-
-
-
-
-    // Box template
-
-    addEmpbox = () => {
-
-        let span = document.createElement('span');
-        let ul1 = document.createElement('ul');
-        let ul2 = document.createElement('ul');
-        let ul3 = document.createElement('ul');
-
-        let li1 = document.createElement('li');
-        li1.setAttribute('id', 'empName');
-        li1.setAttribute('class', 'container title is-5-fullhd');
-
-        let li2 = document.createElement('li');
-        li2.setAttribute('id', 'empPos');
-        li2.setAttribute('class', 'container title is-5-fullhd');
-
-        let li3 = document.createElement('li');
-        li3.setAttribute('id', 'empID');
-        li3.setAttribute('class', 'box');
-
-        let li4 = document.createElement('li');
-        li4.setAttribute('id', 'empEmail');
-        li4.setAttribute('class', 'box');
-
-        let li5 = document.createElement('li');
-        li5.setAttribute('id', 'uniqueVar');
-        li5.setAttribute('class', 'box');
-
-        let li6 = document.createElement('li');
-
-        let li7 = document.createElement('li');
-
-
-        //
-        ul2.append(li1);
-        ul2.append(li2);
-        ul3.append(li3);
-        ul3.append(li4);
-        ul3.append(li5);
-        li6.append(ul2);
-        li7.append(ul3);
-        ul1.append(li6);
-        ul1.append(li7);
-
-
-        let div = document.createElement('div');
-        div.setAttribute('class', 'box is-primary column is-one-fifth is-bordered');
-
-        div.append(ul1);
-        return div;
-
-    };
-}
 
 
 
